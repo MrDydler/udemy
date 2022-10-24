fb02565042f1d7e0c109914eac90b1fba87aca19
Dusha <mrdydlera@gmail.com>
2022-10-25 01:28:55 +0500

    first commit

---
+++ UdemyCourse/script.js
@@ -0,0 +1,19 @@
+//alert('hello');
+//const result = confirm("ты тут?");
+//console.log(result);
+//const answer = prompt ('Вам есть 18?', "18");
+//alert('Вам есть '+ answer +' заходите!');
+//console.log(answer);
+
+//const answers = [];
+//answers[0] = prompt('как ваше имя?', '');
+//answers[1] = prompt('как ваша фамилия?', '');
+//answers[2] = prompt('сколько Вам лет', '');
+
+//console.log(answers);
+
+"use strict";
+const category = 'toys';
+console.log(`https://someurk/${category}/5`);
+const user = "ivan";
+alert(`Привет, ${user}`);
